2d8682da8e9a70c357270daaf083e774430532ed
Usagi Ito <usagi@WonderRabbitProject.net>
2013-05-15 01:35:33 +0900

    alpha-2

--- include/WonderRabbitProject/SQLite3.hpp
+++ include/WonderRabbitProject/SQLite3.hpp
@@ -18,8 +18,20 @@ namespace WonderRabbitProject
     }
 
     const std::string version        = SQLITE_VERSION;
-    const size_t      version_number = SQLITE_VERSION_NUMBER;
+    const uint32_t    version_number = SQLITE_VERSION_NUMBER;
     const std::string source_id      = SQLITE_SOURCE_ID;
+
+    inline std::string sqlite3_version()
+    { throw std::logic_error("not impl; use the other API."); }
+    
+    inline std::string sqlite3_libversion()
+    { return C::sqlite3_libversion(); }
+    
+    inline std::string sqlite3_sourceid()
+    { return C::sqlite3_sourceid(); }
+    
+    inline uint32_t libversion_number()
+    { return C::sqlite3_libversion_number(); }
 
     #ifndef WRP_SQLITE3_NO_UNDEF    
       #undef SQLITE_VERSION
@@ -73,6 +85,9 @@ namespace WonderRabbitProject
       #undef SQLITE_OPEN_WAL
     #endif
     
+    constexpr OPEN_FLAGS operator|(const OPEN_FLAGS a, const OPEN_FLAGS b)
+    { return OPEN_FLAGS( int(a) | int(b) ); }
+
     enum class RESULT_CODE : int
     { OK         =  0
     , ERROR      =  1
@@ -137,46 +152,218 @@ namespace WonderRabbitProject
       #undef SQLITE_DONE
     #endif
     
-    struct sqlite3_t final
+    inline void validate(RESULT_CODE r)
     {
-      sqlite3_t()
-      { open(":memory:"); }
-      sqlite3_t(const std::string & filename)
-      { open(filename); }
-      ~sqlite3_t()
-      { close(); }
+      if( r != RESULT_CODE::OK )
+        throw std::runtime_error
+          ( u8"FAIL; RESULT_CODE is " + std::to_string(int(r)) );
+            // ToDo: to_string(RESULT_CODE)
+    }
+    
+    struct zeroblob_size_t final
+    {
+      explicit zeroblob_size_t(size_t size__) noexcept
+        : size_(size__)
+      { }
+      size_t size() const
+      { return size_; }
     private:
-      void open(const std::string & filename)
+      const size_t size_;
+    };
+    
+    struct sqlite3_t;
+    
+    struct prepare_t final
+    {
+      friend class sqlite3_t;
+      
+      ~prepare_t()
       {
-        auto r = C::sqlite3_open(filename.data(), &pd);
-        if(r)
-          throw std::runtime_error
-              ( std::string("open failed: ") + std::to_string(r) );
+        auto r = RESULT_CODE( C::sqlite3_finalize(ps) );
+        validate(r);
       }
       
-      void open(const std::string & filename, OPEN_FLAGS flags)
+      void step() const
       {
-        auto r = C::sqlite3_open_v2
-                   ( filename.data()
-                   , &pd
-                   , int(flags)
-                   , nullptr
-                   );
-        if(r)
-          throw std::runtime_error
-              ( std::string("open_v2 failed: ") + std::to_string(r) );
-        
-      void close()
+        auto r = RESULT_CODE( C::sqlite3_step(ps) );
+        validate(r);
+      }
+      
+      void bind
+      ( const std::vector<uint8_t> & blob
+      , const int index = 1
+      ) const
       {
-        C::sqlite3_close(pd);
-        if(r)
-          throw std::runtime_error
-              ( std::string("close failed: ") + std::to_string(r) );
+        auto r = RESULT_CODE
+        ( C::sqlite3_bind_blob
+          ( ps
+          , index
+          , blob.data()
+          , blob.size()
+          , nullptr
+          )
+        );
+        validate(r);
+      }
+
+      void bind( const double v, const int index = 1 ) const
+      {
+        auto r = RESULT_CODE( C::sqlite3_bind_double(ps, index, v) );
+        validate(r);
+      }
+
+      void bind( const int32_t v, const int index = 1 ) const
+      {
+        auto r = RESULT_CODE( C::sqlite3_bind_int(ps, index, v) );
+        validate(r);
+      }
+
+      void bind( const int64_t v, const int index = 1 ) const
+      {
+        auto r = RESULT_CODE( C::sqlite3_bind_int64(ps, index, v) );
+        validate(r);
+      }
+
+      void bind( decltype(nullptr), const int index = 1 ) const
+      {
+        auto r = RESULT_CODE( C::sqlite3_bind_null(ps, index) );
+        validate(r);
+      }
+
+      void bind( const std::string & text, const int index = 1 ) const
+      {
+        auto r = RESULT_CODE
+        ( C::sqlite3_bind_text
+          ( ps
+          , index
+          , text.data()
+          , text.size()
+          , nullptr
+          )
+        );
+        validate(r);
+      }
+
+      void bind( const std::u16string & text, const int index = 1 ) const
+      {
+        auto r = RESULT_CODE
+        ( C::sqlite3_bind_text16
+          ( ps
+          , index
+          , text.data()
+          , text.size()
+          , nullptr
+          )
+        );
+        validate(r);
+      }
+
+      void bind( const C::sqlite3_value & v, const int index = 1 ) const
+      {
+        auto r = RESULT_CODE( C::sqlite3_bind_value(ps, index, &v) );
+        validate(r);
+      }
+
+      void bind( zeroblob_size_t v, const int index = 1 ) const
+      {
+        assert( v.size() > std::numeric_limits<int>::max() );
+        auto r = RESULT_CODE
+        ( C::sqlite3_bind_zeroblob
+          ( ps
+          , index
+          , int(v.size())
+          )
+        );
+        validate(r);
+      }
+
+      void reset() const
+      {
+        auto r = RESULT_CODE( C::sqlite3_reset(ps) );
+        validate(r);
+      }
+      
+    private:
+      
+      prepare_t(C::sqlite3_stmt * ps_)
+        : ps(ps_)
+      { }
+      
+      C::sqlite3_stmt * ps;
+    };
+  
+    struct sqlite3_t final
+    {
+      static constexpr OPEN_FLAGS default_open_flags
+        = OPEN_FLAGS::READWRITE
+        | OPEN_FLAGS::CREATE
+        ;
+      
+      sqlite3_t
+      ( const std::string & filename = u8":memory:"
+      , OPEN_FLAGS flags = default_open_flags
+      )
+      { open(filename, flags); }
+      
+      ~sqlite3_t()
+      { close(); }
+      
+      prepare_t prepare(const std::string & sql) const
+      {
+        C::sqlite3_stmt* ps;
+        auto r = RESULT_CODE
+        ( C::sqlite3_prepare_v2
+          ( pd
+          , sql.data()
+          , -1
+          , &ps
+          , nullptr
+          )
+        );
+        validate(r);
+        return prepare_t(ps);
+      }
+      
+      prepare_t prepare(const std::u16string & sql) const
+      {
+        C::sqlite3_stmt* ps;
+        auto r = RESULT_CODE
+        ( C::sqlite3_prepare16_v2
+          ( pd
+          , sql.data()
+          , -1
+          , &ps
+          , nullptr
+          )
+        );
+        validate(r);
+        return prepare_t(ps);
+      }
+      
+    private:
+      
+      inline void open(const std::string & filename, OPEN_FLAGS flags)
+      {
+        auto r = RESULT_CODE
+        ( C::sqlite3_open_v2
+          ( filename.data()
+          , &pd
+          , int(flags)
+          , nullptr
+          )
+        );
+        validate(r);
+      }
+      
+      inline void close()
+      {
+        auto r = RESULT_CODE( C::sqlite3_close(pd) );
+        validate(r);
       }
       
       C::sqlite3 * pd = nullptr;
     };
-  
+    
   }
 }
 
